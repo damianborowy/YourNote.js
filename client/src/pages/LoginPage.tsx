0ed1f7e839c2f4698ced47677dd36c4c1fb46048
lehovitz <michallechowicz14@gmail.com>
2020-03-23 16:06:44 +0100

    Implemented authentication on front
    added pages and communication with back through endpoints

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import history from "history";
+import { Redirect } from "react-router-dom";
+import styles from "./LoginPage.module.scss";
+import Container from "@material-ui/core/Container";
+import { Grid } from "@material-ui/core";
+import userApi from "../apis/UserAPI";
+import Alert from "@material-ui/lab/Alert";
+
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import ApiResponse from "../apis/ApiResponse";
+
+interface ILoginPageState {
+  email: string;
+  password: string;
+  redirect: boolean;
+  isEmailValid: boolean;
+  res: ApiResponse;
+  loggedIn: boolean;
+}
+class LoginPage extends Component {
+  state: Readonly<ILoginPageState> = {
+    email: "",
+    password: "",
+    redirect: false,
+    loggedIn: false,
+    isEmailValid: true,
+    res: null!
+  };
+
+  handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleLoginButtonClick = async () => {
+    const { email, password } = this.state;
+
+    const response = await userApi.login(email, password);
+    this.setState({ res: response });
+    //if (response.success || localStorage.getItem("jwt")) {
+    if (response.success) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  };
+
+  handleRegisterButtonClick = () => {
+    this.setState({ redirect: true });
+  };
+
+  renderErrorr = () => {
+    return;
+  };
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/register"></Redirect>;
+    }
+  };
+
+  renderLoggedCorectly = () => {
+    if (this.state.loggedIn) {
+      return <Redirect to="/loggedIn"></Redirect>;
+    } else if (this.state.res) {
+      return (
+        <Alert severity="error">
+          <AlertTitle>Incorrect credentials</AlertTitle>
+          {this.state.res.message}
+        </Alert>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className={styles.containerParent}>
+        <Grid container spacing={2} className={styles.container}>
+          <Grid item xs={12}>
+            {this.renderLoggedCorectly()}
+
+            <h1 className={styles.headers}>Your Note</h1>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              className={styles.textField}
+              type="email"
+              required
+              label="e-mail"
+              variant="filled"
+              onChange={this.handleEmailChange}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              className={styles.textField}
+              required
+              type="password"
+              label="Password"
+              variant="filled"
+              onChange={this.handlePasswordChange}
+            ></TextField>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              className={styles.button}
+              onClick={this.handleLoginButtonClick}
+            >
+              Login
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            {this.renderRedirect()}
+            <Button
+              variant="contained"
+              onClick={this.handleRegisterButtonClick}
+              className={styles.button}
+            >
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,190 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Redirect } from "react-router-dom";
+import styles from "./RegisterPage.module.scss";
+import Grid from "@material-ui/core/Grid";
+import userApi from "../apis/UserAPI";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import ApiResponse from "../apis/ApiResponse";
+import { Router, withRouter } from "react-router-dom";
+
+interface IRegisterPageState {
+  email: string;
+  password: string;
+  confirmed: string;
+  goBack: boolean;
+  regSucc: ApiResponse;
+}
+class RegisterPage extends Component {
+  state: Readonly<IRegisterPageState> = {
+    email: "",
+    password: "",
+    confirmed: "",
+    goBack: false,
+    regSucc: null!
+  };
+  handleLoginChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ email: event.target.value });
+  };
+  handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      password: event.target.value,
+      isPasswordValid: this.validatePassword()
+    });
+  };
+
+  handleConfirmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      confirmed: event.target.value,
+      isPasswordValid: this.validatePassword()
+    });
+  };
+  handleRegisterButtonClick = async () => {
+    const { email, password } = this.state;
+
+    const response = await userApi.register(email, password);
+
+    if (response.success) {
+      this.setState({ goBack: true });
+    } else {
+      this.setState({ regSucc: response });
+    }
+  };
+
+  handleGoBackButtonClick = () => {
+    this.setState({ goBack: true });
+  };
+
+  validateMail = () => {
+    var re = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
+    return re.test(this.state.email);
+  };
+
+  renderGoBack = () => {
+    if (this.state.goBack) {
+      if (this.state.regSucc) {
+        if (this.state.regSucc.success) {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { param: this.state.regSucc }
+              }}
+            ></Redirect>
+          );
+        }
+      }
+      return <Redirect to="/"></Redirect>;
+    }
+  };
+
+  renderAlertErr = () => {
+    if (this.state.regSucc) {
+      if (!this.state.regSucc.success) {
+        return (
+          <Alert severity="error">
+            <AlertTitle>Error</AlertTitle>
+            {this.state.regSucc.message}
+          </Alert>
+        );
+      }
+    }
+  };
+
+  validatePassword = () => {
+    if (this.state.password === this.state.confirmed) {
+      return true;
+    }
+    return false;
+  };
+
+  render() {
+    return (
+      <div className={styles.containerParent}>
+        <Grid container spacing={2} className={styles.container}>
+          {this.renderAlertErr()}
+          <Grid item xs={12}>
+            <h1 className={styles.headers}>Your Note</h1>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              className={styles.textField}
+              id="filled-basic"
+              label="e-mail"
+              variant="filled"
+              onChange={this.handleLoginChange}
+              type="email"
+              error={!this.validateMail() && this.state.email.length !== 0}
+              helperText={
+                !this.validateMail() && this.state.email.length !== 0
+                  ? "Email address is invalid."
+                  : ""
+              }
+              required
+            ></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type="password"
+              className={styles.textField}
+              id="filled-basic"
+              label="Password"
+              variant="filled"
+              onChange={this.handlePasswordChange}
+              required
+            ></TextField>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              type="password"
+              error={!this.validatePassword()}
+              className={styles.textField}
+              id="filled-error-helper-text"
+              label="Confirm password"
+              helperText={
+                !this.validatePassword() ? "Passwords don't match." : ""
+              }
+              required
+              variant="filled"
+              onChange={this.handleConfirmChange}
+            ></TextField>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              className={styles.button}
+              onClick={this.handleRegisterButtonClick}
+              disabled={
+                !this.validatePassword() ||
+                !this.validateMail ||
+                this.state.password.length === 0 ||
+                this.state.email.length === 0 ||
+                (!this.validateMail() && this.state.email.length !== 0)
+              }
+            >
+              Register
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            {this.renderGoBack()}
+            <Button
+              className={styles.buttonLink}
+              variant="outlined"
+              color="primary"
+              onClick={this.handleGoBackButtonClick}
+            >
+              sign in instead
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default RegisterPage;

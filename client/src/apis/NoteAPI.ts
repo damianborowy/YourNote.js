b19664335eef27f99eaf57ff856c13b44aa34fd8
Lehovitz <m.lechowicz@icloud.com>
2020-03-30 03:23:20 +0200

    Implemented basic NoteAPI

---
+++ client/src/apis/NoteAPI.ts
@@ -0,0 +1,14 @@
+import ApiResponse from "./ApiResponse";
+import Note from "../models/Note";
+
+const noteApi = {
+    async create(note: Note) {},
+
+    async read() {},
+
+    async update(note: Note) {},
+
+    async delete(noteId: string) {}
+};
+
+export default noteApi;

38c4f0a19d18b604f06c25deb02e79831b430f0b
Damian <d.m.borowy@gmail.com>
2020-03-30 03:15:13 +0200

    Added note model on front

---
+++ client/src/models/Note.ts
@@ -0,0 +1,12 @@
+export default class Note {
+    constructor(
+        public _id: string,
+        public title: string,
+        public content: string,
+        public color: number,
+        public ownerId: string,
+        public date: Date,
+        public tags: string,
+        public sharedTo: string[]
+    ) {}
+}
